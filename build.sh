5ba8a175d327fede5def82bb533cb9caa82ec833
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-09-16 07:18:35 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,14 +9,20 @@ test -n "${AWX_BRANCH}" || {
 
 git clone -b $AWX_BRANCH https://github.com/ansible/awx.git awx-${AWX_BRANCH}
 
+pip3 install docker-compose ansible
+
 (
+    echo "----- build -------------------------------------------------------- "
+
     cd awx-${AWX_BRANCH}
 
-    python3 -m venv venv
-    source venv/bin/activate
-
-    pip3 install wheel setuptools_scm docker-compose ansible
-
     export PYTHON=python3
+    export VENV_BASE=$(pwd)/venv_base
+
+    make requirements_dev
     make docker-compose-build SHELL=/bin/bash
+#    python3 -m venv venv
+#    source venv/bin/activate
+#    pip3 install docker-compose ansible
+
 )
