691963859b93512676b4dbfe942dc493259a354e
jaybanuan <40902617+jaybanuan@users.noreply.github.com>
2022-10-20 00:13:08 +0900

    Update minikube-prepare.sh

--- minikube-prepare.sh
+++ minikube-prepare.sh
@@ -2,7 +2,7 @@ eval $(minikube docker-env)
 
 docker pull quay.io/ansible/awx-ee:latest
 docker pull redis:7
-docker pull quay.io/ansible/awx-operator:0.29.0
-docker pull quay.io/ansible/awx:21.6.0
+docker pull quay.io/ansible/awx-operator:0.30.0
+docker pull quay.io/ansible/awx:21.7.0
 docker pull postgres:13
 docker pull gcr.io/kubebuilder/kube-rbac-proxy:v0.13.0
